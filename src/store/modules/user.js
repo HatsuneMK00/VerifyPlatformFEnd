9c9d214ae96696bb60f63e2791b43c741527e13d
makise <10175101106@stu.ecnu.edu.cn>
2021-07-12 23:45:41 +0800

    feat: working fine with backend

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -34,6 +34,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
+        console.log(data.token)
         commit('SET_TOKEN', data.token)
         setToken(data.token)
         resolve()
