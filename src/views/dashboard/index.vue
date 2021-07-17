c3a899f41a2537493fdcbaed6fc0312b9af42e4e
makise <10175101106@stu.ecnu.edu.cn>
2021-07-17 18:33:03 +0800

    feat: fetch and save userId finished, wait to connect with back end

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">name: {{ name }}, userId: {{ userId }}</div>
   </div>
 </template>
 
@@ -11,7 +11,8 @@ export default {
   name: 'Dashboard',
   computed: {
     ...mapGetters([
-      'name'
+      'name',
+      'userId'
     ])
   }
 }
